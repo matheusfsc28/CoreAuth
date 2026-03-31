06d3abad75dcfe6486b1597247bfcd3e1ff3f752
Matheus Felipe <matheus.felipescruz@gmail.com>
2026-03-30 22:49:08 -0300

    FEAT: Adding infrastructure dependency injection

---
+++ src/CoreAuth.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,58 @@
+﻿using CoreAuth.Domain.Interfaces.Data;
+using CoreAuth.Domain.Interfaces.Repositories.Base;
+using CoreAuth.Domain.Interfaces.Repositories.Tokens;
+using CoreAuth.Domain.Interfaces.Repositories.Users;
+using CoreAuth.Infrastructure.Data;
+using CoreAuth.Infrastructure.Repositories.Base;
+using CoreAuth.Infrastructure.Repositories.Tokens;
+using CoreAuth.Infrastructure.Repositories.Users;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CoreAuth.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            AddRepositories(services);
+            AddUnitOfWork(services);
+
+            AddDbContext(services, configuration);
+
+            return services;
+        }
+
+        private static void AddRepositories(IServiceCollection services)
+        {
+            services.AddScoped(typeof(IBaseReadRepository<>), typeof(BaseRepository<>));
+            services.AddScoped(typeof(IBaseWriteRepository<>), typeof(BaseRepository<>));
+
+            services.AddScoped<IPasswordResetTokenReadRepository, PasswordResetTokenRepository>();
+            services.AddScoped<IPasswordResetTokenWriteRepository, PasswordResetTokenRepository>();
+
+            services.AddScoped<IRefreshTokenReadRepository, RefreshTokenRepository>();
+            services.AddScoped<IRefreshTokenWriteRepository, RefreshTokenRepository>();
+
+            services.AddScoped<IUserReadRepository, UserRepository>();
+            services.AddScoped<IUserWriteRepository, UserRepository>();
+        }
+
+        private static void AddUnitOfWork(IServiceCollection services)
+        {
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+        }
+
+        private static void AddDbContext(IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("PostgresqlConnection");
+
+            services.AddDbContext<CoreAuthDbContext>(opt =>
+            {
+                opt.UseNpgsql(connectionString);
+                opt.UseSnakeCaseNamingConvention();
+            });
+        }
+    }
+}
